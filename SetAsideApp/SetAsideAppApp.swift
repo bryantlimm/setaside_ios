8b96c4ed964dc62bdd53ac352970525fb7e596a4
bryantlimm <142878856+bryantlimm@users.noreply.github.com>
2025-09-24 18:28:04 +0700

    Initial Commit

---
+++ SetAsideApp/SetAsideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetAsideAppApp.swift
+//  SetAsideApp
+//
+//  Created by Bryant Aryadi on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SetAsideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
